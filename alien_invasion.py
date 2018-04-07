aadc5ebff09093848ffa541e41bf95dbc2da9ba4
Volodymyr Yahello <vjagello93@gmail.com>
2018-04-07 14:52:27 +0300

    Introduce alien invesion game

---
+++ alien_invasion.py
@@ -0,0 +1,55 @@
+import pygame
+from pygame.sprite import Group
+from alien_invasion import game_functions
+from alien_invasion.settings import Settings
+from alien_invasion.ship import Ship
+from alien_invasion.game_stats import GameStats
+from alien_invasion.button import Button
+from alien_invasion.scoreboard import Scoreboard
+
+
+def _run_game():
+    """ Main module to run the game."""
+
+    # Initialize pygame, Settings and screen objects
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+                                      ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Create an instance to store game statistics and create a scoreboard.
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # Make the Play button.
+    play_button = Button(screen, "Play")
+
+    # Make a ship, group of bullets and group of aliens
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create a fleet of aliens.
+    game_functions.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Starting main loop for the game
+    while True:
+
+        game_functions.check_events(ai_settings, screen, stats, sb, play_button, ship,
+                                    aliens, bullets)
+
+        if stats.game_active:
+            bullets.update()
+            game_functions.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
+                                          bullets)
+            game_functions.update_aliens(ai_settings, screen, stats, sb, ship, aliens,
+                                         bullets)
+            ship.update()
+
+        game_functions.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets,
+                                     play_button)
+
+
+if __name__ == '__main__':
+    _run_game()
---
+++ alien_invasion/ship.py
@@ -0,0 +1,56 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Ship(Sprite):
+    """ A class representing a singgle instance of a ship."""
+    def __init__(self, ai_settings, screen):
+        """ Initializing the screen and set it's starting position."""
+        super().__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the ship and get it's rect
+        self.image = pygame.image.load("alien_invasion/images/ship.png")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start new ship at the center-bottom of the screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for ship's center.
+        # Pygame rect attributes only store integer values.
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.centerx = self.screen_rect.centerx
+        self.centery = self.screen_rect.bottom - 20
+
+    def update(self):
+        """ Update ship's position based on movement flag """
+        # Update the ship's center value not the rect.
+        if self.moving_right and (self.rect.right < self.screen_rect.right):
+            self.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and (self.rect.left > 0):
+            self.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_up and (self.rect.top > 0):
+            self.centery -= self.ai_settings.ship_speed_factor
+        if self.moving_down and (self.rect.bottom < self.screen_rect.bottom):
+            self.centery += self.ai_settings.ship_speed_factor
+
+        # Update rect value from self.center.
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
+
+    def blitme(self):
+        """ Draw the ship at it's current position. """
+        self.screen.blit(self.image, self.rect)
